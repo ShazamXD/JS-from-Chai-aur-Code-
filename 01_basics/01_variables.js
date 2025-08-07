aebc1d20c121146f7d354fbba9f9971ab893d3dd
Chitransh Pal <140273638+ShazamXD@users.noreply.github.com>
2025-08-07 19:07:14 +0000

    Variables and Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=14453
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur" // possible to declare variable like this but should never do not good practice 
+let accountState; // this will display undefined in console because the variable is only declared and not alloted any value 
+
+// accountId=2 // not allowed since const
+
+/*
+prefer not to use var datatype because of issue in block scope and functional scope use let instead a better choice 
+*/
+
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict" // when you use this now all JS code will be treated as newer version and also it is not necessary to use semicolon in JS if u can just change lines 
+
+// number : range from pow(2,53)
+// null : standalone value when u intentionally assign null value 
+// undefined : when u only declare the variable and assigns no value 
+
+
+console.log(typeof undefined) // output comes out to be undefined 
+console.log(typeof null) // output comes out to be object 
