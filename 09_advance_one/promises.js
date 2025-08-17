5517d096bc8a3459a9f4f06a4d7e4229116bb2f0
ShazamXD <chitransh.pal108@gmail.com>
2025-08-17 18:19:31 +0530

    Promises_one

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,36 @@
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is compelete');
+        resolve()
+    }, 1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "Chai", email: "chai@example.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user); // since resolve and then are connected so we can use user as parameter in them
+})
+
+
+
+
