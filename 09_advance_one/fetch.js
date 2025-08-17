79368ed221d83ae0d4f4056f49da97a8f141216a
ShazamXD <chitransh.pal108@gmail.com>
2025-08-17 20:11:39 +0530

    fetch

---
+++ 09_advance_one/fetch.js
@@ -0,0 +1,18 @@
+// fetch() ko bdiya se pdhne ke liye mdn docs pr zaroor pde wha na smj aaye toh Lecture 41 dekh lo 
+
+/* when fetch() is called it performs two functions simultaneously
+
+- it handles API on web browsers/node
+- it reserves memory space for data in private variables called resolve and reject these are not accessible to us 
+
+network request is acknowledges then send to resolve if not then reject
+
+if resolve then data is extracted and it gets stored in variable 'response'  (stores the data that server send)
+
+NOTE: also if after network request "ERROR 404",etc appears then they will also store in resolve array not in reject array because
+      server did response amd displayed these messages. Reject happens only when network down ho, internet na ho, ya request bhejna hi possible na ho
+
+
+FOR MORE DEPTH KNOWLEDGE CONSIDER MDN DOCS AND LECTURE 41 CHAI AUR CODE
+
+*/
--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -32,5 +32,82 @@ promiseThree.then(function(user){
 })
 
 
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username: "hitesh", password: "123"})
+        }else{
+            reject(`ERROR: Something went wrong`)
+        }
+    }, 1000)
+})
+
+promiseFour.then(function(user){
+    console.log(user);
+    return user.username
+}).then(function(username){
+    console.log(username); // you can use as many then and catch as you want (chaining) it's just that the lower then will return upper then value 
+}).catch(function(error){
+    console.log(error); // Error handling ke liye catch use hota hai. and jb resolve ho jaata hai to then use hota hai
+}).finally(function(){
+    console.log("The promise is either resolved or rejected") // 'finally' this executes no matter what happens
+})
+
+
+// Another way of promises using Async-Await
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "javascript", password: "123"})
+        }else{
+            reject(`ERROR: JS went wrong`)
+        }
+    },1000)
+})
+
+async function consumerPromiseFive(){ // always returns a promise 
+    try{
+        const response = await promiseFive // waits till the promise is resolved 
+        console.log(response);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+consumerPromiseFive() // same whi kaam krta hai async-await bhi bss syntax alag hai jo shi lg rha woh yaad kr lo 
+
+
+// https://jsonplaceholder.typicode.com/users
+
+async function getAllUsers(){
+    try{
+        const response = await fetch ('https://jsonplaceholder.typicode.com/users')
+
+        const data = await response.json()
+        console.log(data)
+    }catch(error){
+        console.log("E: ", error);
+    }
+}
+getAllUsers();
+
+
+// By using then and catch
+
+fetch('https://jsonplaceholder.typicode.com/users') // fetch immediately ek Promise<Response> return karta hai aur background me HTTP GET request bhejta hai.
+.then((response) => {
+    return response.json() // since string ke form mein by default data aata hai so to change them into json 
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+
+
+
 
 
