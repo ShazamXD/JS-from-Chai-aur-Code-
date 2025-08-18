81cfe44104cf7b010465cd44bf84e09136756251
ShazamXD <chitransh.pal108@gmail.com>
2025-08-18 11:28:34 +0530

    oops

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,21 @@
+# Javascript and Classes
+
+## OOP  
+- collection of properties and methods 
+- toLowerCase is an example
+
+## why use OOP
+-JavaScript khud ek multi-paradigm language hai → matlab isme aap procedural, functional, aur OOP sabhi tareeke se code likh sakte ho.
+
+## parts of OOPs
+Object Literal
+- Constrcutor Function
+- Prototypes
+- Classes
+- Instances (new, this, etc)
+
+## 4 pillars of OOPs
+- Abstraction: cheeze chipa do example: fetch()
+- Encapsulation: wrapper lga do
+- Inheritance: inherit properties from parent
+- Polymorphism: bahut saare roop ek function ke 
---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,49 @@
+const UserProfile = {
+    username: "Hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database"); 
+        // console.log(`Username: ${this.username}`); 
+        /* if you would have written only username without 'this' 
+        keyword then i would have given an error that username not defined. Since, we know that ecah function
+        created it's own execution context in js engine and to excess values from outside we use .this keyword
+        for indicating current context variables/data. Exampel: Monday toh monday hai pr kis week ka monday ??
+        so that is told using .this keyword current context */
+        
+        // console.log(this); // will print current context details 
+    }
+}
+    // console.log(User.username)
+    // console.log(User.getUserDetails());
+    // console.log(this); /* this will give empty object because global context contains nothing as of now but
+                          //  in case of browser since there is Window object 
+
+
+// CONSTRUCTOR FUNCTION ('new' keyword in js is a constructor function) provides new instance(empty object) everytime
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false) /* if both userOne and userTwo were not declared using 'new' keyword then they would have 
+overwritten each other so that's why new keyword provides them a copy on which they can work on seperately and individually */
+console.log(userOne.constructor); // print function 'User' 
+//console.log(userTwo); 
+
+
+
+
+
+
