14c793d2ad98aef8ec7e3780229394e7692cac76
ShazamXD <chitransh.pal108@gmail.com>
2025-08-21 18:01:38 +0530

    getter and setter

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,9 +1,26 @@
+// getter and setter works together if one is used then other must also have to be used for proper functioning 
+
 class User {
     constructor(email, password){
         this.email = email;
         this.password = password
     }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
 }
 
-const hitesh = new User("h@hitesh.ai", "123")
-console.log(hitesh.password);
+const hitesh = new User("h@hitesh.ai", "abc")
+console.log(hitesh.email);
